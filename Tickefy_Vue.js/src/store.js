86e4b592e5b43e95b3a575214174535c0129a6e9
neowara <32764514+neowara@users.noreply.github.com>
2019-02-20 15:30:35 +0100

    Manually merged the work we did with Johan in school and did some modifications in Tickefy_Vue.js/Events,Admin.vue (made it possible to post to the database), Event.vue. And in Tickefy_API, installed cors so that the API was able to communicate with Vue in the local machine.

--- Tickefy_Vue.js/src/store.js
+++ Tickefy_Vue.js/src/store.js
@@ -1,16 +1,51 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    event: {},
+    events: [],
+    tickets: [],
+    verifyData: null
   },
   mutations: {
-
+    selectEvent(state, event){
+        state.event = event;
+    },
+    setTickets(state, tickets){
+        state.tickets = tickets;
+    },
+    setEvents(state, events){
+        state.events = events;
+    },
+    setVerifyData(state, data){
+      state.verifyData = data;
+    }
   },
   actions: {
+    async getEvents(ctx){
 
+        let events = await axios.get('http://localhost:3000/events');
+        ctx.commit('setEvents', events.data);
+
+    },
+    async buy(ctx, buyData){
+        
+        let tickets = await axios.post('http://localhost:3000/tickets', buyData);
+        ctx.commit('setTickets', tickets.data);
+        localStorage.setItem('tickets', JSON.stringify(tickets.data));
+
+    },
+    getTickets(ctx){
+        let tickets = localStorage.getItem('tickets');
+        ctx.commit('setTickets', JSON.parse(tickets));
+    },
+    async verifyTicket(ctx, code){
+      let verification = await axios.get(`http://localhost:3000/verify/${code}`);
+      ctx.commit('setVerifyData', verification.data);
+    }
   }
 })
---
+++ Tickefy_Vue.js/src/views/Pay.vue
@@ -0,0 +1,139 @@
+<template>
+    <main id="pay">
+        <section class="content" v-if="event">
+        <p>You are about to score some sweet tickets to</p>
+        <h1>{{ event.name }}</h1>
+        <p class="when">{{ event.when.date }} {{ event.when.from }} - {{ event.when.to }}</p>
+        <p class="where">@ Globen</p>
+        <section class="counter">
+          <article class="price">{{ event.price * amount }} sek</article>
+          <article class="decrese" @click=" amount-- ">
+            <img src="../assets/decrese.svg" alt="decrese">
+          </article>
+          <article class="num-tickets">{{ amount }}</article>
+          <article class="increse" @click=" amount++ ">
+            <img src="../assets/increse.svg" alt="increse">
+          </article>
+        </section>
+        <a href="#" class="btn" @click="buy">Take my money!</a>
+        </section>
+        <section class="content" v-if="!event">
+          <p>No ticket selected.</p>
+          <a href="#" class="btn" @click="$router.push('/events')">Go to events</a>
+        </section>
+        <pager :activeStep="activeStep"/>
+    </main>
+</template>
+
+<script>
+import pager from '@/components/Pager';
+export default {
+  name: 'pay',
+  data(){
+    return {
+      activeStep: 2,
+      amount: 1
+    }
+  },
+  components: {
+    pager
+  },
+  computed: {
+    event(){
+      return this.$store.state.event;
+    }
+  },
+  methods: {
+    buy(){
+      this.$store.dispatch('buy', { event: this.event._id, amount: this.amount });
+      this.$router.push('/tickets');
+    
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import '../scss/variables';
+#pay {
+  background: $darkblue;
+  display: flex;
+  flex-direction: column;
+  .content {
+    flex: 7;
+    @extend %center;
+    flex-direction: column;
+    a {
+      &.btn {
+        max-width: 80vw;
+        width: 100%;
+        border-radius: 3px;
+        color: white;
+        text-decoration: none;
+        font-size: 1.2rem;
+        font-weight: 700;
+        background: $green;
+        height: 4rem;
+        @extend %center;
+      }
+    }
+    p {
+      color: rgba($color: #fff, $alpha: .7);
+      font-size: 1.2rem;
+      font-style: italic;
+      padding: 2rem 3rem;
+      text-align: center;
+    }
+    h1 {
+      color: $pink;
+      font-size: 2.4rem;
+      text-align: center;
+      line-height: 2.4rem;
+      margin: .5rem 2.6rem;
+      text-shadow: 1px 1px 0 rgba($color: #fff, $alpha: .1); 
+    }
+    .when {
+      font-size: 1.2rem;
+      font-weight: 700;
+      color: $green;
+      text-align: center;
+      margin: 0;
+      padding: .25rem 0;
+    }
+    .where {
+      font-size: 1.2rem;
+      color: rgba($color: #fff, $alpha: .6);
+      text-align: center;
+      margin: 0;
+      padding: 0;
+    }
+    .counter {
+      max-width: 14rem;
+      width: 100%;
+      margin: 2rem 0;
+      border: .125rem solid $pink;
+      border-radius: 4px;
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-auto-rows: 4rem;
+      font-size: 1.6rem;
+      color: white;
+      .price {
+        grid-column: 1/4;
+        @extend %center;
+        border-bottom: 1px solid $pink;
+      }
+      .increse {
+        @extend %center;
+      }
+      .decrese {
+        @extend %center;
+      }
+      .num-tickets {
+        @extend %center;
+        font-weight: 700;
+        font-size: 1.8rem;
+      }
+    }
+  }
+}
+</style>
